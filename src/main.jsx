c23a56f17a89444d54615a951fdaa19ef6b52b9f
Mariana <mariana.urcuqui01@gmail.com>
2025-03-12 21:35:00 -0500

    dist to build

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-const router = createBrowserRouter([
-  {
-    path: '/website-project//*',
-    element:<App />,
-  }
-])
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+// const router = createBrowserRouter([
+//   {
+//     path: '/website-project//*',
+//     element:<App />,
+//   }
+// ])
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <RouterProvider router={router} />
+// );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter basename="/website-project">
+        <App />
+    </BrowserRouter>
 );
-
